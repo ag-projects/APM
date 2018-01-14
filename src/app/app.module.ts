d4e72b4a0df0775b92cf7416faf0e54471f1b1c5
Armen G <gharibi.armen@yahoo.com>
2018-01-13 18:25:40 -0800

    Added product interface, custom pipes, custom filter to product list page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { productListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    productListComponent
+    productListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     BrowserModule,
